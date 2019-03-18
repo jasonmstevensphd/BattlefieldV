8b572cb3cddeba6ab8efc6bd1c199e50b4eee6c0
jasonmstevensphd <jasonmstevensphd@gmail.com>
2019-03-17 21:20:45 -0400

    derp

--- Game_Generator.R
+++ Game_Generator.R
@@ -3,7 +3,7 @@ Games <- c("Grand Operations", "Rush", "Breakthrough", "Conquest", "Frontlines")
 
 Classes <- c("Assault", "Support", "Medic", "Recon")
 
-set.seed(573)
+set.seed(574)
 
 sample(Games, 1)
 
